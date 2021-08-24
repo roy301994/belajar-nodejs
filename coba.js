8d0d86dcf8405330afcc8c9678f32e9e78c1bb4f
Roy Sihotang <roynaldosihotang@gmail.com>
2021-08-24 15:00:06 +0700

    first commit

---
+++ coba.js
@@ -0,0 +1,22 @@
+// console.log('hello wpu')
+
+function cetakNama (nama ){
+    return `halo nama saya adalah ${nama}`
+}
+
+//Perbedaan cetak nama dan consolelog cetak nama adalah
+// cetakNama('roy gelllss') => menyimpan ke memori
+// console.log(cetakNama('roy gelllss')) // => mencetak ke layar
+
+module.exports=cetakNama
+
+
+// function mencetakNamaSaya (parameter){
+//     return `halo saya bernama ${parameter}`
+// }
+
+
+// module.exports=mencetakNamaSaya
+
+
+// console.log(mencetakNamaSaya('ardi'))
---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+//menjalankan 2 console log di 2 file js yg berbeda
+const cetakNama=require ('./coba')
+console.log('hello roy')
+console.log (cetakNama('hello broo'))
